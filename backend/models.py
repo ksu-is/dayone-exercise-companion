1e84ae605c3722ff38626cefc6bf5c3e6846a689
Russell White <russgw@icloud.com>
2025-04-13 16:41:00 -0500

    Add files via upload
    updated models.py script to include youtube links for form demonstrations

--- backend/models.py
+++ backend/models.py
@@ -1,27 +1,61 @@
 from django.db import models
 
-class Exercise(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    duration = models.PositiveIntegerField(help_text="Duration in minutes", blank=True, null=True)
+MUSCLE_GROUP = (
+    ("calves", "Calves"),
+    ("hamstrings", "Hamstrings"),
+    ("quads", "Quadriceps(quads)"),
+    ("glutes", "Glutes"),
+    ("biceps", "Biceps"),
+    ("triceps", "Triceps"),
+    ("forearms", "Forearms"),
+    ("traps", "Trapezius(traps)"),
+)
 
+
+class Exercise(models.Model):
+    # Attributes
+    user = models.ForeignKey
+    name = models.CharField(null=False, blank=False, max_length=100)
+    tips = models.TextField(null=False, blank=True, max_length=1000)
+    youtube_link = models.URLField(null=False, blank=True, max_length=2000)
+    # Methods
     def __str__(self):
         return self.name
 
 
 class ExerciseSet(models.Model):
-    name = models.CharField(max_length=100)
-    exercises = models.ManyToManyField(Exercise, related_name="sets")
-    created_at = models.DateTimeField(auto_now_add=True)
+    # Attributes for exercise sets
+    user = models.ForeignKey
+    exercise = models.ForeignKey(
+        Exercise, null=False, blank=False, on_delete=models.CASCADE
+    )
+    sets = models.ManyToManyField("exercises.Set", related_name="sets")
+    number_of_sets = models.IntegerField(null=False, blank=False, default=3)
 
+    # Methods
     def __str__(self):
-        return self.name
+        return str(self.exercise.name) + " - " + str(self.number_of_sets) + " sets"
 
 
 class Set(models.Model):
-    exercise_set = models.ForeignKey(ExerciseSet, on_delete=models.CASCADE, related_name="sets")
-    repetitions = models.PositiveIntegerField()
-    rest_time = models.PositiveIntegerField(help_text="Rest time in seconds", blank=True, null=True)
+    # Constants
+    user = models.ForeignKey
+    REPS_UNIT = (("RE", "Reps"),)
+    WEIGHT_UNIT = (("KG", "Kg."), ("BW", "Body Weight"))
+    # Attributes
+    reps = models.PositiveIntegerField(null=False, blank=False, default=0)
+    reps_unit = models.CharField(max_length=2, choices=REPS_UNIT, default="RE")
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    weight_unit = models.CharField(max_length=2, choices=WEIGHT_UNIT, default="RE")
 
+    # Methods
     def __str__(self):
-        return f"{self.exercise_set.name} - {self.repetitions} reps"
+        return (
+            str(self.reps)
+            + " "
+            + str(self.reps_unit)
+            + " x "
+            + str(self.weight)
+            + " "
+            + str(self.weight_unit)
+        )
